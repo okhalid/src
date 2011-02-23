a6d5ff11237a9e085f7844e4812f0e53419336cd
Omer Khalid <okhalid.cern@gmail.com>
2011-02-23 02:07:31 +0100

    PyUtil Repository Initialized

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+#for trivial example
+#from setuptools import setup
+
+# for distutils distribution
+from distutils.core import setup
+
+setup(name='pyutil', 
+      version='0.1.1',
+      description='Useful Python Modules',
+      maintainer='Omer Khalid',
+      maintainer_email='okhalid.cern@gmail.com',
+      platforms=["any"],
+      license="http://www.gnu.org/licenses/gpl.html",
+     )
